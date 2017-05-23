657bec19212c2b5c9887037b6e2fa4b49f7e8984
Artem42rus <artbannikov@yandex.ru>
2017-05-23 21:56:57 +0300

    Calculator V.0.3

--- Calculator.cpp
+++ Calculator.cpp
@@ -31,12 +31,23 @@ int Mul ()
 	result = a * b;
 	cout << result << endl;
 }
+
+float Div ()
+{
+	float a, b;
+	float result;
+	cout << "¬ведите 2 числа: ";
+	cin >> a >> b;
+	result = a / b;
+	cout << result << endl;	
+}
 int main ()
 {
 	setlocale (0, "");
 	Sum ();
 	Sub ();
 	Mul ();
+	Div ();
 	system ("pause");
 	return 0;
 }
